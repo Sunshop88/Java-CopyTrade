603e7bb09f7436304665ce2911a70df47b58d55d
zsd <2225695391@qq.com>
2024-11-20 09:53:14 +0800

    修改vps统计接口处理 总持仓 总手数 总净值 总盈亏均为跟单账号

--- follow-order-followcom/src/main/java/net/maku/followcom/service/impl/FollowVpsServiceImpl.java
+++ follow-order-followcom/src/main/java/net/maku/followcom/service/impl/FollowVpsServiceImpl.java
@@ -3,23 +3,18 @@ package net.maku.followcom.service.impl;
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSONObject;
-import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.fhs.trans.service.impl.TransService;
 import jakarta.servlet.http.HttpServletRequest;
-import kotlin.jvm.internal.Lambda;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.maku.followcom.convert.FollowVpsConvert;
 import net.maku.followcom.dao.FollowVpsDao;
-import net.maku.followcom.entity.ClientEntity;
 import net.maku.followcom.entity.FollowTraderEntity;
 import net.maku.followcom.entity.FollowVpsEntity;
 import net.maku.followcom.enums.CloseOrOpenEnum;
@@ -41,7 +36,6 @@ import net.maku.framework.common.utils.ExcelUtils;
 import net.maku.framework.common.utils.PageResult;
 import net.maku.framework.common.utils.RandomStringUtil;
 import net.maku.framework.mybatis.service.impl.BaseServiceImpl;
-import org.json.HTTP;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -51,6 +45,7 @@ import java.text.MessageFormat;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * vps列表
@@ -84,9 +79,9 @@ public class FollowVpsServiceImpl extends BaseServiceImpl<FollowVpsDao, FollowVp
         return new PageResult<>(followVpsVOS, page.getTotal());
     }
 
-    private LambdaQueryWrapper<FollowVpsEntity> getWrapper(FollowVpsQuery query){
+    private LambdaQueryWrapper<FollowVpsEntity> getWrapper(FollowVpsQuery query) {
         LambdaQueryWrapper<FollowVpsEntity> wrapper = Wrappers.lambdaQuery();
-        wrapper.eq(FollowVpsEntity::getDeleted,query.getDeleted());
+        wrapper.eq(FollowVpsEntity::getDeleted, query.getDeleted());
         return wrapper;
     }
 
@@ -104,8 +99,8 @@ public class FollowVpsServiceImpl extends BaseServiceImpl<FollowVpsDao, FollowVp
     public Boolean save(FollowVpsVO vo) {
         vo.setClientId(RandomStringUtil.generateUUIDClientId());
         FollowVpsEntity entity = FollowVpsConvert.INSTANCE.convert(vo);
-        List<FollowVpsEntity> list = this.list(new LambdaQueryWrapper<FollowVpsEntity>().eq(FollowVpsEntity::getIpAddress, vo.getIpAddress()).or().eq(FollowVpsEntity::getName,vo.getName()));
-        if (ObjectUtil.isNotEmpty(list)){
+        List<FollowVpsEntity> list = this.list(new LambdaQueryWrapper<FollowVpsEntity>().eq(FollowVpsEntity::getIpAddress, vo.getIpAddress()).or().eq(FollowVpsEntity::getName, vo.getName()));
+        if (ObjectUtil.isNotEmpty(list)) {
             throw new ServerException("重复名称或ip地址,请重新输入");
         }
         baseMapper.insert(entity);
@@ -117,9 +112,9 @@ public class FollowVpsServiceImpl extends BaseServiceImpl<FollowVpsDao, FollowVp
     public void update(FollowVpsVO vo) {
         FollowVpsEntity entity = FollowVpsConvert.INSTANCE.convert(vo);
         FollowVpsVO followVpsVO = this.get(Long.valueOf(vo.getId()));
-        if (ObjectUtil.notEqual(vo.getName(),followVpsVO.getName())){
-            List<FollowVpsEntity> list = this.list(new LambdaQueryWrapper<FollowVpsEntity>().eq(FollowVpsEntity::getName,vo.getName()));
-            if (ObjectUtil.isNotEmpty(list)){
+        if (ObjectUtil.notEqual(vo.getName(), followVpsVO.getName())) {
+            List<FollowVpsEntity> list = this.list(new LambdaQueryWrapper<FollowVpsEntity>().eq(FollowVpsEntity::getName, vo.getName()));
+            if (ObjectUtil.isNotEmpty(list)) {
                 throw new ServerException("重复名称,请重新输入");
             }
         }
@@ -158,7 +153,7 @@ public class FollowVpsServiceImpl extends BaseServiceImpl<FollowVpsDao, FollowVp
 
     @Override
     public void export() {
-    List<FollowVpsExcelVO> excelList = FollowVpsConvert.INSTANCE.convertExcelList(list());
+        List<FollowVpsExcelVO> excelList = FollowVpsConvert.INSTANCE.convertExcelList(list());
         transService.transBatch(excelList);
         ExcelUtils.excelExport(FollowVpsExcelVO.class, "vps列表", null, excelList);
     }
@@ -174,54 +169,64 @@ public class FollowVpsServiceImpl extends BaseServiceImpl<FollowVpsDao, FollowVp
 
     @Override
     public List<FollowVpsEntity> listByVpsName(List<String> vps) {
-        return list(new LambdaQueryWrapper<FollowVpsEntity>().in(FollowVpsEntity::getName,vps).orderByDesc(FollowVpsEntity::getCreateTime));
+        return list(new LambdaQueryWrapper<FollowVpsEntity>().in(FollowVpsEntity::getName, vps).orderByDesc(FollowVpsEntity::getCreateTime));
     }
 
     @Override
     public void transferVps(Integer oldId, HttpServletRequest req) {
         FollowVpsVO followVpsEntityOld = this.get(Long.valueOf(oldId));
         //发送请求到旧VPS，清除缓存
-        String url = MessageFormat.format("http://{0}:{1}{2}", followVpsEntityOld.getIpAddress(),FollowConstant.VPS_PORT,FollowConstant.VPS_TRANSFERVPS);
+        String url = MessageFormat.format("http://{0}:{1}{2}", followVpsEntityOld.getIpAddress(), FollowConstant.VPS_PORT, FollowConstant.VPS_TRANSFERVPS);
         JSONObject body = RestUtil.request(url, HttpMethod.GET, RestUtil.getHeaderApplicationJsonAndToken(req), null, null, JSONObject.class).getBody();
-        log.info("旧VPS清理缓存请求:"+body);
+        log.info("旧VPS清理缓存请求:" + body);
     }
 
     @Override
     public void startNewVps(Integer newId, HttpServletRequest req) {
         FollowVpsVO followVpsEntityOld = this.get(Long.valueOf(newId));
         //发送请求到新VPS，启动账号
-        String url = MessageFormat.format("http://{0}:{1}{2}", followVpsEntityOld.getIpAddress(),FollowConstant.VPS_PORT,FollowConstant.VPS_STARTNEWVPS);
+        String url = MessageFormat.format("http://{0}:{1}{2}", followVpsEntityOld.getIpAddress(), FollowConstant.VPS_PORT, FollowConstant.VPS_STARTNEWVPS);
         JSONObject body = RestUtil.request(url, HttpMethod.GET, RestUtil.getHeaderApplicationJsonAndToken(req), null, null, JSONObject.class).getBody();
-        log.info("新VPS启动账号请求:"+body);
+        log.info("新VPS启动账号请求:" + body);
     }
 
     @Override
     public FollowVpsEntity select(String vpsName) {
-        return baseMapper.selectOne(Wrappers.<FollowVpsEntity>lambdaQuery().eq(FollowVpsEntity::getName,vpsName));
+        return baseMapper.selectOne(Wrappers.<FollowVpsEntity>lambdaQuery().eq(FollowVpsEntity::getName, vpsName));
     }
 
     @Override
     public FollowVpsInfoVO getFollowVpsInfo(FollowTraderService followTraderService) {
         List<FollowVpsEntity> list = this.list();
-        Integer openNum = (int)list.stream().filter(o->o.getIsOpen().equals(CloseOrOpenEnum.OPEN.getValue())).count();
-        Integer runningNum = (int)list.stream().filter(o->o.getIsActive().equals(CloseOrOpenEnum.OPEN.getValue())).count();
-        Integer closeNum = (int)list.stream().filter(o->o.getIsActive().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
-        Integer errorNum = (int)list.stream().filter(o->o.getConnectionStatus().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
+        Integer openNum = (int) list.stream().filter(o -> o.getIsOpen().equals(CloseOrOpenEnum.OPEN.getValue())).count();
+        Integer runningNum = (int) list.stream().filter(o -> o.getIsActive().equals(CloseOrOpenEnum.OPEN.getValue())).count();
+        Integer closeNum = (int) list.stream().filter(o -> o.getIsActive().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
+        Integer errorNum = (int) list.stream().filter(o -> o.getConnectionStatus().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
         //账号信息
         List<FollowTraderEntity> followTraderEntityList = followTraderService.list();
-        Integer masterSuccess =(int)followTraderEntityList.stream().filter(o->o.getType().equals(TraderTypeEnum.MASTER_REAL.getType())&&o.getStatus().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
-        Integer masterTotal =(int) followTraderEntityList.stream().filter(o->o.getType().equals(TraderTypeEnum.MASTER_REAL.getType())).count();
-        Integer slaveSuccess = (int)followTraderEntityList.stream().filter(o->o.getType().equals(TraderTypeEnum.SLAVE_REAL.getType())&&o.getStatus().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
-        Integer slaveTotal = (int) followTraderEntityList.stream().filter(o->o.getType().equals(TraderTypeEnum.SLAVE_REAL.getType())).count();
-        Integer orderCountTotal =0;
-        BigDecimal orderLotsTotal =BigDecimal.ZERO;
-        BigDecimal orderEquityTotal =BigDecimal.ZERO;
-        for(FollowTraderEntity followTraderEntity:followTraderEntityList){
-            if (ObjectUtil.isNotEmpty(redisUtil.get(Constant.TRADER_USER + followTraderEntity.getId()))){
-                FollowRedisTraderVO followRedisTraderVO =(FollowRedisTraderVO) redisUtil.get(Constant.TRADER_USER + followTraderEntity.getId());
-                orderCountTotal+=followRedisTraderVO.getTotal();
-                orderLotsTotal=orderLotsTotal.add(BigDecimal.valueOf(followRedisTraderVO.getSellNum())).add(BigDecimal.valueOf(followRedisTraderVO.getBuyNum()));
-                orderEquityTotal=orderEquityTotal.add(followRedisTraderVO.getEuqit());
+        Integer masterSuccess = (int) followTraderEntityList.stream().filter(o -> o.getType().equals(TraderTypeEnum.MASTER_REAL.getType()) && o.getStatus().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
+        Integer masterTotal = (int) followTraderEntityList.stream().filter(o -> o.getType().equals(TraderTypeEnum.MASTER_REAL.getType())).count();
+        Integer slaveSuccess = (int) followTraderEntityList.stream().filter(o -> o.getType().equals(TraderTypeEnum.SLAVE_REAL.getType()) && o.getStatus().equals(CloseOrOpenEnum.CLOSE.getValue())).count();
+        Integer slaveTotal = (int) followTraderEntityList.stream().filter(o -> o.getType().equals(TraderTypeEnum.SLAVE_REAL.getType())).count();
+        Integer orderCountTotal = 0;
+        BigDecimal orderLotsTotal = BigDecimal.ZERO;
+        BigDecimal orderEquityTotal = BigDecimal.ZERO;
+        BigDecimal orderProfitTotal = BigDecimal.ZERO;
+        //过滤出为跟单的
+        List<FollowTraderEntity> slaveTraderEntityList = followTraderEntityList.stream().filter(o -> o.getType().equals(TraderTypeEnum.SLAVE_REAL.getType())).collect(Collectors.toList());
+        //followTraderEntityList 替换出slaveTraderEntityList
+        for (FollowTraderEntity followTraderEntity : slaveTraderEntityList) {
+            if (ObjectUtil.isNotEmpty(redisUtil.get(Constant.TRADER_USER + followTraderEntity.getId()))) {
+                FollowRedisTraderVO followRedisTraderVO = (FollowRedisTraderVO) redisUtil.get(Constant.TRADER_USER + followTraderEntity.getId());
+                //总持仓
+                orderCountTotal += followRedisTraderVO.getTotal();
+                //总持仓手数
+                orderLotsTotal = orderLotsTotal.add(BigDecimal.valueOf(followRedisTraderVO.getSellNum())).add(BigDecimal.valueOf(followRedisTraderVO.getBuyNum()));
+                //总净值
+                orderEquityTotal = orderEquityTotal.add(followRedisTraderVO.getEuqit());
+                //总盈亏
+                orderProfitTotal = orderProfitTotal.add(followRedisTraderVO.getProfit());
+
             }
         }
         Integer total = list.size();
@@ -235,9 +240,14 @@ public class FollowVpsServiceImpl extends BaseServiceImpl<FollowVpsDao, FollowVp
         followVpsInfoVO.setMasterTotal(masterTotal);
         followVpsInfoVO.setSlaveSuccess(slaveSuccess);
         followVpsInfoVO.setSlaveTotal(slaveTotal);
+        //总持仓
         followVpsInfoVO.setOrderCountTotal(orderCountTotal);
+        //总持仓手数
         followVpsInfoVO.setOrderLotsTotal(orderLotsTotal);
+        //总净值
         followVpsInfoVO.setOrderEquityTotal(orderEquityTotal);
+        //总盈亏
+        followVpsInfoVO.setOrderProfitTotal(orderProfitTotal);
         return followVpsInfoVO;
     }
 }
--- follow-order-followcom/src/main/java/net/maku/followcom/vo/FollowVpsInfoVO.java
+++ follow-order-followcom/src/main/java/net/maku/followcom/vo/FollowVpsInfoVO.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 /**
  * vps信息
@@ -16,42 +15,45 @@ import java.time.LocalDateTime;
 @Data
 @Schema(description = "vps信息")
 public class FollowVpsInfoVO implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Schema(description = "总数")
-	private Integer total;
+    @Schema(description = "总数")
+    private Integer total;
 
-	@Schema(description = "对外开放数量")
-	private Integer openNum;
+    @Schema(description = "对外开放数量")
+    private Integer openNum;
 
-	@Schema(description = "运行中数量")
-	private Integer runningNum;
+    @Schema(description = "运行中数量")
+    private Integer runningNum;
 
-	@Schema(description = "关闭数量")
-	private Integer closeNum;
+    @Schema(description = "关闭数量")
+    private Integer closeNum;
 
-	@Schema(description = "异常数量")
-	private Integer errorNum;
+    @Schema(description = "异常数量")
+    private Integer errorNum;
 
-	@Schema(description = "策略正常数量")
-	private Integer masterSuccess;
+    @Schema(description = "策略正常数量")
+    private Integer masterSuccess;
 
-	@Schema(description = "策略总数量")
-	private Integer masterTotal;
+    @Schema(description = "策略总数量")
+    private Integer masterTotal;
 
-	@Schema(description = "跟单正常数量")
-	private Integer slaveSuccess;
+    @Schema(description = "跟单正常数量")
+    private Integer slaveSuccess;
 
-	@Schema(description = "跟单总数量")
-	private Integer slaveTotal;
+    @Schema(description = "跟单总数量")
+    private Integer slaveTotal;
 
-	@Schema(description = "总持仓单量")
-	private Integer orderCountTotal;
+    @Schema(description = "总持仓单量")
+    private Integer orderCountTotal;
 
-	@Schema(description = "总持仓手数")
-	private BigDecimal orderLotsTotal;
+    @Schema(description = "总持仓手数")
+    private BigDecimal orderLotsTotal;
 
-	@Schema(description = "总净值")
-	private BigDecimal orderEquityTotal;
+    @Schema(description = "总净值")
+    private BigDecimal orderEquityTotal;
+	
+    @Schema(description = "总盈亏")
+    private BigDecimal orderProfitTotal;
 
 }
