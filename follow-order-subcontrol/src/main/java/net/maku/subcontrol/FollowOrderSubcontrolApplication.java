2b7c1ee8876165e2b08ccbb53c8a83256fb85896
LLL <2455854101@qq.com>
2024-11-27 16:42:14 +0800

    feat(主控): 添加每周测速任务功能

--- follow-order-subcontrol/src/main/java/net/maku/subcontrol/FollowOrderSubcontrolApplication.java
+++ follow-order-subcontrol/src/main/java/net/maku/subcontrol/FollowOrderSubcontrolApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"net.maku.framework","net.maku.followcom", "net.maku.subcontrol"})
 @MapperScan(basePackages = {"net.maku.followcom.dao","net.maku.subcontrol.dao"})
 @EnableAsync
+@EnableScheduling
 public class FollowOrderSubcontrolApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(FollowOrderSubcontrolApplication.class, args);
