de61f0e678e8c02148fdcf8daa98ae854734f93d
sunyong <570539529@qq.com>
2024-12-02 13:53:36 +0800

    fix(异常修复)

--- follow-order-followcom/src/main/java/net/maku/followcom/config/CacheConfig.java
+++ follow-order-followcom/src/main/java/net/maku/followcom/config/CacheConfig.java
@@ -1,24 +1,56 @@
 package net.maku.followcom.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    public CacheManager caffeineCacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-                .maximumSize(500) // 最大缓存条目数
-                .expireAfterWrite(10, TimeUnit.DAYS)); // 写入 10天
-        return cacheManager;
+    public CacheManager cacheManager(@Qualifier("redisConnectionFactory1") RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofDays(10));  // 设置过期时间为 10 天
+
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(@Qualifier("redisConnectionFactory1") RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        configureSerialization(template);
+        return template;
+    }
+
+    private void configureSerialization(RedisTemplate<String, Object> template) {
+        // Key 和 HashKey 使用 String 序列化
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+
+        // Value 和 HashValue 使用 Json 序列化
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.afterPropertiesSet();
     }
 }
