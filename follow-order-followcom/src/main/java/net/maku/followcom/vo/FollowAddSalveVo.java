366a13cd282aa86a7303a0f40d618a7ae39a38a3
sunyong <570539529@qq.com>
2025-01-22 10:41:01 +0800

    fix(子控): 下单更新可跟单下单,注释类型可为空

--- follow-order-followcom/src/main/java/net/maku/followcom/vo/FollowAddSalveVo.java
+++ follow-order-followcom/src/main/java/net/maku/followcom/vo/FollowAddSalveVo.java
@@ -96,8 +96,6 @@ public class FollowAddSalveVo  implements Serializable {
     private String fixedComment;
 
     @Schema(description = "注释类型0-英文 1-数字 2-英文+数字+符号")
-    @Min(value = 0, message = "注释类型参数不合法")
-    @Max(value = 2, message = "注释类型参数不合法")
     private Integer commentType;
 
     @Schema(description = "位数")
