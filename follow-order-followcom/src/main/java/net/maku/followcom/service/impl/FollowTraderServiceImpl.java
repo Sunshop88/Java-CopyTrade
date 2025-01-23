758320d7f46556f825042e2ae74bcd4c0143c808
LLL <2455854101@qq.com>
2025-01-23 19:48:39 +0800

    feat(服务器): 账号重登

--- follow-order-followcom/src/main/java/net/maku/followcom/service/impl/FollowTraderServiceImpl.java
+++ follow-order-followcom/src/main/java/net/maku/followcom/service/impl/FollowTraderServiceImpl.java
@@ -1697,7 +1697,7 @@ public class FollowTraderServiceImpl extends BaseServiceImpl<FollowTraderDao, Fo
         //根据名称查询列表信息
         LambdaQueryWrapper<FollowTraderEntity> queryWrapper = Wrappers.lambdaQuery();
         queryWrapper.eq(FollowTraderEntity::getPlatform, name)
-                .eq(FollowTraderEntity::getStatus, CloseOrOpenEnum.CLOSE.getValue()
+                .eq(FollowTraderEntity::getIpAddr, FollowConstant.LOCAL_HOST
                 );
         // 执行查询并返回结果
         return baseMapper.selectList(queryWrapper);
