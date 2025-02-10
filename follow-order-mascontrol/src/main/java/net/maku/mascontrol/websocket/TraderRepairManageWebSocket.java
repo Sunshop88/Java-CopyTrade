35e3296f20909c280434355ef2a02ffc6f36b1df
zsd <2225695391@qq.com>
2025-02-10 17:01:31 +0800

    fix(修改redis序列化):

--- follow-order-mascontrol/src/main/java/net/maku/mascontrol/websocket/TraderRepairManageWebSocket.java
+++ follow-order-mascontrol/src/main/java/net/maku/mascontrol/websocket/TraderRepairManageWebSocket.java
@@ -154,7 +154,7 @@ public class TraderRepairManageWebSocket {
               Map<String, Map<Object, Object>> closeMap = redisUtil.getKeysByThreeConditions("follow:repair:close:*", o.getIpAddress(), trader.getPlatform(), trader.getAccount());
               Integer salvenum = getOrder(orderRepairInfoVOList, sendMap, closeMap, trader.getId(),o.getIpAddress());*/
 
-              Map<String, Object> objectObjectMap = redisCache.hGetAll(Constant.REPAIR_SEND+trader.getAccount()+":"+trader.getId());
+              Map<Object, Object> objectObjectMap = redisCache.hGetStrAll(Constant.REPAIR_SEND+trader.getAccount()+":"+trader.getId());
                 if(objectObjectMap!=null){
                     objectObjectMap.values().forEach(obj->{
                         JSONObject jsonObject = JSONObject.parseObject(obj.toString());
