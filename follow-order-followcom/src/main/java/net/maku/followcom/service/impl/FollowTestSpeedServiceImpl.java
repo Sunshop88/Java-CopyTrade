bd717fea96eeda98060e567a71fa08313ea5de91
LLL <2455854101@qq.com>
2025-02-11 00:32:00 +0800

    feat(节点测速): 修改测速

--- follow-order-followcom/src/main/java/net/maku/followcom/service/impl/FollowTestSpeedServiceImpl.java
+++ follow-order-followcom/src/main/java/net/maku/followcom/service/impl/FollowTestSpeedServiceImpl.java
@@ -328,11 +328,8 @@ public class FollowTestSpeedServiceImpl extends BaseServiceImpl<FollowTestSpeedD
                     newEntity.setTestId(testId);
                     newEntity.setTestUpdateTime(measureTime);
                     newEntity.setIsDefaultServer(defaultServerNode.equals(serverNode.getServerNode() + ":" + serverNode.getServerPort()) ? 1 : 0);
-                    log.info("保存前测速结果：{}", newEntity);
-
-//                    newEntity.setServerUpdateTime(localDateTime != null ? localDateTime : null);
+                    newEntity.setServerUpdateTime(localDateTime != null ? localDateTime : null);
 //                    newEntity.setServerUpdateTime(localDateTime != null ? LocalDateTime.parse(DateUtil.format(localDateTime, "yyyy-MM-dd HH:mm:ss")) : null);
-                    log.info("保存后测速结果：{}", newEntity);
                     // 获取最新的服务器更新时间
 //                    List<FollowTestDetailVO> vo = (List<FollowTestDetailVO>) redisUtil.get(Constant.VPS_NODE_SPEED + "detail");
 //                    List<FollowTestDetailVO> detailVOList = vo.stream()
@@ -356,7 +353,6 @@ public class FollowTestSpeedServiceImpl extends BaseServiceImpl<FollowTestSpeedD
 
                     // 将结果加入到待保存列表
                     entitiesToSave.add(newEntity);
-                    log.info("大小：{}", entitiesToSave.size());
                 });
             }
         }
