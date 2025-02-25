6992db748f7d182bb30c747fc27462964f3d0d67
zsd <2225695391@qq.com>
2025-02-25 10:06:47 +0800

    feat(交易):

--- follow-order-followcom/src/main/java/net/maku/followcom/vo/BargainVO.java
+++ follow-order-followcom/src/main/java/net/maku/followcom/vo/BargainVO.java
@@ -1,14 +1,25 @@
 package net.maku.followcom.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 /**
  * Author:  zsd
  * Date:  2025/2/24/周一 16:58
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BargainVO {
     //持仓概览
     private BargainOverviewVO bargainOverviewVO;
     //账号列表
-
-    //持仓列表
+     private  BargainAccountVO accountVO;
+    //持仓订单集合
+    private List<OrderActiveInfoVO> orderActiveInfoList;
 
 }
--- follow-order-mascontrol/src/main/java/net/maku/mascontrol/websocket/BargainWebSocket.java
+++ follow-order-mascontrol/src/main/java/net/maku/mascontrol/websocket/BargainWebSocket.java
@@ -1,16 +1,29 @@
 package net.maku.mascontrol.websocket;
 
+import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
 import net.maku.followcom.service.DashboardService;
 import net.maku.followcom.util.SpringContextUtils;
+import net.maku.followcom.vo.BargainAccountVO;
+import net.maku.followcom.vo.OrderActiveInfoVO;
+import net.maku.framework.common.cache.RedisCache;
+import net.maku.framework.common.constant.Constant;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.rmi.ServerException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -29,6 +42,8 @@ public class BargainWebSocket {
     private final DashboardService dashboardService = SpringContextUtils.getBean(DashboardService.class);
     private ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
     private Map<String, ScheduledFuture<?>> scheduledFutureMap = new HashMap<>();
+    private RedisCache redisCache = SpringContextUtils.getBean(RedisCache.class);
+
     @OnOpen
     public void onOpen(Session session) throws IOException {
     }
@@ -39,14 +54,33 @@ public class BargainWebSocket {
             String id = session.getId();
             JSONObject jsonObj = JSONObject.parseObject(message);
             JSONArray accountIds = jsonObj.getJSONArray("accountIds");
-             ScheduledFuture<?> st = scheduledFutureMap.get(id);
+            String currentAccountId = jsonObj.getString("currentAccountId");
+
+
+            ScheduledFuture<?> st = scheduledFutureMap.get(id);
             if (st != null) {
                 st.cancel(true);
             }
           ScheduledFuture scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(() -> {
             try {
-                JSONObject json = new JSONObject();
-                session.getBasicRemote().sendText(json.toJSONString());
+                BargainAccountVO bargainAccountVO = new BargainAccountVO();
+                //选中当前账号的持仓
+                if(ObjectUtil.isNotEmpty(currentAccountId)) {
+                Object o1 = redisCache.get(Constant.TRADER_ACTIVE + currentAccountId);
+                List<OrderActiveInfoVO> orderActiveInfoList =new ArrayList<>();
+                if (ObjectUtil.isNotEmpty(o1)){
+                    orderActiveInfoList = JSONObject.parseArray(o1.toString(), OrderActiveInfoVO.class);
+
+                }
+                bargainAccountVO.setOrderActiveInfoList(orderActiveInfoList);
+                }
+                ObjectMapper objectMapper = new ObjectMapper();
+                JavaTimeModule javaTimeModule = new JavaTimeModule();
+                //格式化时间格式
+                javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                objectMapper.registerModule(javaTimeModule);
+                String s = objectMapper.writeValueAsString(bargainAccountVO);
+                session.getBasicRemote().sendText(s);
             } catch (Exception e) {
                 e.printStackTrace();
                 log.error("推送异常:{}", e);
