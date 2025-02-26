8d73e006706cafcb20ea831d4501c4be43fd34a2
zsd <2225695391@qq.com>
2025-02-26 18:43:38 +0800

    feat(账号列表统计):

---
+++ follow-order-followcom/src/main/java/net/maku/followcom/vo/TraderUserStatVO.java
@@ -0,0 +1,25 @@
+package net.maku.followcom.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Author:  zsd
+ * Date:  2025/2/26/周三 18:12
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TraderUserStatVO {
+    //账号总数量
+    private int total;
+    //未挂靠vps
+    private int noVpsNum;
+    //连接成功
+    private int conNum;
+    //连接异常
+    private int errNum;
+}
